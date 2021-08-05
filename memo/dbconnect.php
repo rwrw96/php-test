becd8db44ee8193f98a7ec5cfad7c3a5e1760b8f
rwrw96 <kazuki137@gmail.com>
2021-08-05 06:03:34 +0000

    db名

--- memo/dbconnect.php
+++ memo/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $db = new PDO ('mysql:dbname=mydb;host=localhost;charset=utf8', 
+        $db = new PDO ('mysql:dbname=sumpledb;host=localhost;charset=utf8', 
         'root','root');
     } catch(PDOexception $e) {
         echo 'DB接続エラー' . $e->getMessage();
