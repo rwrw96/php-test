8abad1285df858bc68f23bdd5ff690e56860272e
rwrw96 <kazuki137@gamil.com>
2021-08-06 07:01:10 +0900

    9

--- memo/input.php
+++ memo/input.php
@@ -12,11 +12,11 @@
 </head>
 <body>
 <header>
-<h1 class="font-weight-normal">PHP!!</h1>    
+<h1 class="font-weight-normal">PHP!!!!!</h1>    
 </header>
 
 <main>
-<h2>Practice!!</h2>
+<h2>Practice!!!!!!</h2>
 
 <form action="input_do.php" method="post">
     <textarea name="memo" cols=50 rows=20 placeholder="ご自由にどうぞ"></textarea>
