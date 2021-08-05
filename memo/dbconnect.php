d6e522385412dd6f44a2884995a16f97aa3ddec2
rwrw96 <kazuki137@gamil.com>
2021-08-06 08:15:01 +0900

    15

--- memo/dbconnect.php
+++ memo/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
     try{
         $db = new PDO ('mysql:dbname=mydb;host=mydb.cle5jiuhhe8w.ap-northeast-1.rds.amazonaws.com;charset=utf8', 
-        'root','root');
+        'root','kazuki108');
     } catch(PDOexception $e) {
         echo 'DB接続エラー' . $e->getMessage();
     }
